fab425af32eecaaceed036b985b4b22e05577c81
RajnishPandeyTeqfocus <130439359+RajnishPandeyTeqfocus@users.noreply.github.com>
2024-02-01 22:27:16 +0530

    changes in api

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.post("/api/updateProfile", (req, res) => {
       skills,
     };
 
-    res.status(200).json({ message: "Profile updated successfully!" });
+    res.status(200).json({ message: "Profile updated successfully!!" });
   } catch (error) {
     console.error("Error updating profile:", error);
     res.status(500).json({ message: "Internal Server Error" });
